cb2a79056cfec9f6e2cec628e2ceeab78330082c
riya bhurse <riyabhurse.rb@gmail.com>
2024-07-03 17:52:03 +0530

    First Class

---
+++ Innert1.java
@@ -0,0 +1,97 @@
+package threads;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class NumberPrinter extends Thread{
+  public void run(){
+    for (int i = 1; i <= 10; i++) {
+        try{
+            Thread.sleep(1000);
+      }
+      catch(InterruptedException ex){}
+      System.out.println(i + " " + Thread.currentThread().getName());
+    }
+  }
+}
+class SingleNumberPrinter extends Thread{
+  int num;
+  SingleNumberPrinter(int num){
+    this.num = num;
+  }
+
+  public void run(){
+    num++;
+    System.out.println(num + " " + Thread.currentThread().getName());
+  }
+}
+class HelloWorldPrinter extends Thread  {
+  
+  public void run(){
+    System.out.println("Hello World" + Thread.currentThread().getName());
+  }
+}
+class SingleNumberPrinterV2 implements Runnable{
+  int num;
+  SingleNumberPrinterV2(int num){
+    this.num = num;
+  }
+  @Override
+  public void run(){
+    System.out.println(num + " " + Thread.currentThread().getName());
+  }
+  }
+class DoubleNumberInList extends Thread{
+  ArrayList<Integer> list;
+  DoubleNumberInList(ArrayList<Integer> list){
+    this.list = list;
+  }
+  public void run(){
+    for(int i=0; i<list.size(); i++){
+      list.set(i, list.get(i)*2);
+    }
+    System.out.println(list);
+  }
+
+}
+  public class Innert1 {
+  public static void main(String[] args) {
+        // System.out.println("Hello World" + Thread.currentThread().getName());
+        // HelloWorldPrinter hwp = new HelloWorldPrinter();
+        // hwp.start();
+        // HelloWorldPrinter hwp1 = new HelloWorldPrinter();
+        // hwp1.start();
+        
+        // NumberPrinter np1 = new NumberPrinter();
+        // np1.start();
+        // NumberPrinter np2 = new NumberPrinter();
+        // np2.start();
+
+        // for(int i=1; i<=100; i++){
+        //   SingleNumberPrinter np = new SingleNumberPrinter(i);
+        //   np.start();
+        // }
+        
+        // for(int i=1; i<=100; i++){
+        //   SingleNumberPrinterV2 task = new SingleNumberPrinterV2(i);
+        //   Thread th = new Thread(task);
+        //   th.start();
+        // }
+
+        // ExecutorService es = Executors.newFixedThreadPool(10);
+        // for(int i=1; i<=100; i++){
+        //   es.submit(new SingleNumberPrinterV2(i));
+        // }
+
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=1; i<=10; i++){
+          list.add(i);
+        }  
+
+        DoubleNumberInList dn = new DoubleNumberInList(list);
+        dn.start();
+    }
+}
